fcaa7ac398ccce16e57f02fc77e04f46f4bbc1dd
amisha <amisha.i@canopas.com>
2022-07-26 15:34:53 +0530

    Initial Commit

---
+++ SQLiteExample/SQLiteExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteExampleApp.swift
+//  SQLiteExample
+//
+//  Created by Amisha I on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
